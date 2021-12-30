4444e7b9f3e52c3cb0d2ccdcddb7a785e9a07d8e
Adam Laiacano <adam.laiacano@gmail.com>
2021-12-30 05:15:06 -0500

    cleanup

--- src/2021/09/index.ts
+++ src/2021/09/index.ts
@@ -1,34 +1,15 @@
 import _ from "lodash";
 import internal from "stream";
-import "../../arrayExtensions";
 
 export const part1 = (input: string[]): number => {
   const cells: number[][] = input.map((row) =>
     row.split("").map((cell) => parseInt(cell, 10))
   );
 
-  // function makeKey(row: number, col: number): string {
-  //   return `${row},${col}`;
-  // }
-  // const cellMap: Map<string, number> = new Map();
-  // input.forEach((row, rowIndex) => {
-  //   row.split("").forEach((value, colIndex) => {
-  //     cellMap.set(makeKey(rowIndex, colIndex), parseInt(value, 10));
-  //   });
-  // });
-
   const nCol = input[0].length;
   const nRow = input.length;
   let riskLevel = 0;
 
-  // const testLeft = (col: number, row: number, cellVal: number): boolean =>
-  //   col === 0 || cellMap.getOrElse(makeKey(row, col - 1), 0) > cellVal;
-  // const testRight = (col: number, row: number, cellVal: number): boolean =>
-  //   col === nCol - 1 || cellMap.getOrElse(makeKey(row, col + 1), 0) > cellVal;
-  // const testUp = (col: number, row: number, cellVal: number): boolean =>
-  //   row === 0 || cellMap.getOrElse(makeKey(row - 1, col), 0) > cellVal;
-  // const testDown = (col: number, row: number, cellVal: number): boolean =>
-  //   row === nRow - 1 || cellMap.getOrElse(makeKey(row, col), 0) > cellVal;
   const testLeft = (col: number, row: number, cellVal: number): boolean =>
     col === 0 || cells[row][col - 1] > cellVal;
   const testRight = (col: number, row: number, cellVal: number): boolean =>
